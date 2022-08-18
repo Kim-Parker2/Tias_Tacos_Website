890f34a7f8ad9e1293704b161cc7b9fef9c29dc6
Kimparker2 <92693445+Kimparker2@users.noreply.github.com>
2022-08-18 12:14:12 +1200

    Updating App.js and Index.html

--- tias_tacos/src/App.js
+++ tias_tacos/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import SignUp from "./components/pages/SignUp";
+import WebsiteServices from "./components/pages/WebsiteServices";
+import LogoPage from "./components/pages/LogoPage";
+import PhotoEditingPage from "./components/pages/PhotoEditingPage";
+import StationaryPage from "./components/pages/StationaryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/WebsiteServices' element={<WebsiteServices />} />
+          <Route path='/LogoPage' element={<LogoPage />} />
+          <Route path='/PhotoEditingPage' element={<PhotoEditingPage />} />
+          <Route path='/StationaryPage' element={<StationaryPage />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
-
-export default App;
