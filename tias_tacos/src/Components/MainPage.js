01c902396d81746e8b27af02f4e15d9659b3fc6c
Kimparker2 <92693445+Kimparker2@users.noreply.github.com>
2022-08-23 22:51:20 +1200

    deleted video

--- tias_tacos/src/Components/MainPage.js
+++ tias_tacos/src/Components/MainPage.js
@@ -7,7 +7,7 @@ import './MainPage.css';
 function MainPage() {
   return (
     <div className='mainpage-container'>
-      <video src='/videos/Tia.mp4.mp4' muted autoPlay  />
+      {/* <video src='/videos/Tia.mp4.mp4' muted autoPlay  /> */}
       <h1>WELCOME TO TIA'S TACOS</h1>
       <br></br>
       <br></br>
