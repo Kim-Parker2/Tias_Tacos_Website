5bece4ceaadab43f36b8a3690e648114f4ca7ab7
Kimparker2 <92693445+Kimparker2@users.noreply.github.com>
2022-08-25 15:31:59 +1200

    Changes to logo and frontpage

--- tias_tacos/src/Components/MainPage.js
+++ tias_tacos/src/Components/MainPage.js
@@ -7,14 +7,14 @@ import './MainPage.css';
 function MainPage() {
   return (
     <div className='mainpage-container'>
-      <video src='/videos/Tia.mp4.mp4' muted autoPlay  />
+      <video src='/videos/Tia.mp4' autoPlay  />
       <h1>WELCOME TO TIA'S TACOS</h1>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
 
-      <img src="/images/under-construction.png" alt="" width="330" height="300"/>
+      <img src="/images/undercontraction.png" alt="" width="960" height="330"/>
     
   
    
